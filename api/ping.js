3ab7c48b48f1c36767f327a92b1fdac4d5f50843
marcos-grando <168216213+marcos-grando@users.noreply.github.com>
2025-08-27 01:10:06 -0300

    commit

--- api/ping.js
+++ api/ping.js
@@ -1,4 +1,11 @@
 export default async function handler(req, res) {
     res.setHeader('Content-Type', 'application/json');
-    res.status(200).end(JSON.stringify({ pong: true, url: req.url, method: req.method }));
+    res.status(200).end(JSON.stringify({
+        pong: true,
+        url: req.url,
+        method: req.method,
+        hasHash: !!process.env.ADMIN_PASSWORD_HASH,
+        hasAccess: !!process.env.JWT_ACCESS_SECRET,
+        hasRefresh: !!process.env.JWT_REFRESH_SECRET
+    }));
 };
